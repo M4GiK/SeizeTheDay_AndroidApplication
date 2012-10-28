3c98bca764115bd187267fe81e82833426f4806f
grafen <grafen89@gmail.com>
2012-10-29 00:42:36 +0200

    init. Make new activity

---
+++ src/com/example/endproject/AddComponent.java
@@ -0,0 +1,14 @@
+package com.example.endproject;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AddComponent extends Activity
+{
+	public void onCreate(Bundle savedInstanceState) 
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.add_component);
+	}
+
+}
---
+++ src/com/example/endproject/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.endproject;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.View;
+
+public class MainActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    
+    public void buttonPressed(View v)
+    {
+    	Intent i = new Intent(MainActivity.this,AddComponent.class);
+    	startActivity(i);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
+}

4cfe688db27494d04aeaf5547e92e5a0dc732349
grafen <grafen89@gmail.com>
2012-12-01 14:02:08 +0100

    Add gitignore. Delete some stuff form strings.

--- gen/com/example/endproject/R.java
+++ gen/com/example/endproject/R.java
@@ -35,15 +35,14 @@ public final class R {
         public static final int activity_main=0x7f060000;
     }
     public static final class string {
-        public static final int Add=0x7f040004;
+        public static final int Add=0x7f040003;
         public static final int app_name=0x7f040000;
-        public static final int button_text=0x7f040005;
-        public static final int button_text_cancel=0x7f040007;
-        public static final int button_text_weather=0x7f040006;
-        public static final int hello_world=0x7f040001;
-        public static final int menu_settings=0x7f040002;
-        public static final int set_alarm=0x7f040008;
-        public static final int title_activity_main=0x7f040003;
+        public static final int button_text=0x7f040004;
+        public static final int button_text_cancel=0x7f040006;
+        public static final int button_text_weather=0x7f040005;
+        public static final int menu_settings=0x7f040001;
+        public static final int set_alarm=0x7f040007;
+        public static final int title_activity_main=0x7f040002;
     }
     public static final class style {
         public static final int AppTheme=0x7f050000;

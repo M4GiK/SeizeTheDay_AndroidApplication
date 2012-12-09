c5d925100e8f18fd8f090f972a9faa6e43243535
grafen <grafen89@gmail.com>
2012-12-09 13:48:53 +0100

    fix bug

--- src/com/example/endproject/TaskManager.java
+++ src/com/example/endproject/TaskManager.java
@@ -1,5 +1,8 @@
 package com.example.endproject;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.example.endproject.database.DatabaseHelper;
 
 import android.app.Activity;
@@ -7,9 +10,13 @@ import android.content.ContentValues;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
 
 public class TaskManager extends Activity
 {
@@ -36,12 +43,10 @@ public class TaskManager extends Activity
 	{
 		EditText te = (EditText) findViewById(R.id.editText1);
 		String data = te.getText().toString();
+		te.setText(null);
 		if(data.isEmpty())
 		{
-			if(data=="")
-			{
-				return ;
-			}
+			return ;
 		}
 		
         //write information to data base
@@ -51,11 +56,34 @@ public class TaskManager extends Activity
 		cursor.requery();
 //		Log.d("db","insert new row");
 		refreshView();
+		Log.d("rrrrr","rrrrrrrrrrr");
 	}
 	
 	public void refreshView()
 	{
+		List<String> list = new ArrayList<String>();
+		// get data with query
+        String[] resultColumns = new String[]{"_id","task_text"};
+        cursor = db.query(TABLE_TASK,resultColumns,null,null,null,null,"_id DESC");
+        
+        for(cursor.moveToFirst(); !cursor.isAfterLast(); cursor.moveToNext()) {
+            // The Cursor is now set to the right position
+           list.add(cursor.getString(1));
+        }
+        
+        
+        // add data to adapter
+//		ListAdapter adapter = new SimpleCursorAdapter(this,
+//				R.layout.list_item, cursor,
+//				new String[] {"name", "score"},      // from
+//				new int[] {R.id.name, R.id.score});  // to
+//		// show data in listView
+//		setListAdapter(adapter);
+//		// register listView's context menu
+//		registerForContextMenu(getListView());
+		
 		ListView lw = (ListView) findViewById(R.id.listView1);
-//		lw.add
+//		String r[] = {"aaaa","bbbbb"};
+		lw.setAdapter(new ArrayAdapter<String>(this, R.layout.task_item,list));
 	}
 }
